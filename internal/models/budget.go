43a74c448e70f44fa55aa729fb7ad6d2aeb5e205
danieldgtal <danielchiatuiro@gmail.com>
2024-11-28 23:43:59 -0500

    some lines of code

--- internal/models/budget.go
+++ internal/models/budget.go
@@ -13,8 +13,8 @@ type Budget struct {
 	UserID     uuid.UUID      `gorm:"type:uuid;not null" json:"user_id"`           // Foreign key to the User
 	CategoryID uuid.UUID      `gorm:"type:uuid" json:"category_id"`                // Foreign key to the Category (nullable)
 	Amount     float64        `gorm:"type:decimal(10,2);check:amount >= 0;not null" json:"amount"` // Budget amount
-	StartDate  time.Time           `gorm:"type:date;not null" json:"start_date"`
-	EndDate    time.Time           `gorm:"type:date;not null" json:"end_date"`
+	StartDate  time.Time      `gorm:"type:date;not null" json:"start_date"`
+	EndDate    time.Time      `gorm:"type:date;not null" json:"end_date"`
 	CreatedAt  time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt  time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 	DeletedAt  gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"` // Soft delete
--- internal/tests/healthcheck.go
+++ internal/tests/healthcheck.go
@@ -12,20 +12,3 @@ func HealthCheck(c *gin.Context) {
 		"message": "Expense mgmt Backend is reachable",
 	})
 }
-
-
-
-// func TestHealthCheck(t *testing.T) {
-//     // Create a Gin router with the health check route
-//     router := gin.Default()
-//     AddHealthCheckRoute(router)
-
-//     // Perform a GET request to the /health endpoint
-//     req := httptest.NewRequest(http.MethodGet, "/health", nil)
-//     rec := httptest.NewRecorder()
-//     router.ServeHTTP(rec, req)
-
-//     // Assert the response
-//     assert.Equal(t, http.StatusOK, rec.Code)
-//     assert.JSONEq(t, `{"status":"success","message":"Backend is reachable"}`, rec.Body.String())
-// }
